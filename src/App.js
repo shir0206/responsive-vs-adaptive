e8ee69955270a3d438770cbf42c7c60148a087bc
Shir Zabolotny <40990488+shir0206@users.noreply.github.com>
2020-11-08 16:40:37 +0200

    Update size

--- src/App.js
+++ src/App.js
@@ -6,11 +6,11 @@ export default function App() {
     <div className="cont">
       <div className="responsive">
         <h1>Responsive</h1>
-        <h2>רוחב הכרטיס: 80% מרוחב המסך</h2>
+        <h4>רוחב הכרטיס: 60% מרוחב המסך</h4>
       </div>
       <div className="adaptive">
         <h1>Adaptive</h1>
-        <h2>רוחב הכרטיס: 800 או 300 פיקסלים</h2>
+        <h4>רוחב הכרטיס: 800 או 300 פיקסלים</h4>
       </div>
     </div>
   );
