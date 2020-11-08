b27a5ae07b7f2edb856f69443eff833586b0b4da
Shir Zabolotny <40990488+shir0206@users.noreply.github.com>
2020-11-08 15:06:37 +0200

    Update

--- src/App.js
+++ src/App.js
@@ -3,22 +3,14 @@ import "./styles.css";
 
 export default function App() {
   return (
-    <div>
-      <div class="adaptive">
-        <h1>This is an adaptive container</h1>
-        <p>
-          This container is 800 pixels wide when the browser is any width over
-          500 pixels. Any below that, and the continer shrinks to 300 pixels.
-        </p>
-      </div>
+    <div className="cont">
       <div className="responsive">
-        <h1>ברוכים הבאים :)</h1>
-        <h2>איזו עונה אתם הכי אוהבים?</h2>
+        <h1>Responsive</h1>
+        <h2>רוחב הכרטיס: 80% מרוחב המסך</h2>
       </div>
       <div className="adaptive">
         <h1>Adaptive</h1>
-        <h2>כשרוחב המסך רחב - רוחב הכרטיס 800 פיקסלים</h2>
-        <h2>כשרוחב המסך צר - רוחב הכרטיס 300 פיקסלים</h2>
+        <h2>רוחב הכרטיס: 800 או 300 פיקסלים</h2>
       </div>
     </div>
   );
